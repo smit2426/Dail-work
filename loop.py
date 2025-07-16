eb41391eb9c7b2221e1da5ebb107d9a33f8ddc2b
smit2426 <smit822008@gmail.com>
2025-07-16 12:30:45 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,42 @@
+'''
+text = "PYTHON"
+vowels = "AEIOUaeiou"
+
+for ch in text:
+    if ch in vowels:
+        continue
+    print(ch)
+'''
+'''
+for i in range(10, 0, -1):
+    print(i)
+
+'''
+'''
+for i in range(5, 51, 5):
+    print(i)
+
+'''
+'''
+for i in range(1, 21):
+    if i % 2 != 0:
+        print(i)
+'''
+'''
+i = 1
+while i <= 50:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+'''
+for i in range(1, 11):
+    print(i**2)
+
+
+
+
+
+
+
+ 
+    
