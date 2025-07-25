316b7fdf2b2f1d658a223100bca2aeecc30a10cb
smit2426 <smit822008@gmail.com>
2025-07-25 10:59:12 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,27 @@
+'''
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hii,", last_name + ",", first_name + "!")
+'''
+'''
+item = "apple"
+price = 5.50
+print(f"The price of {item} is {price} dollars.")
+'''
+'''
+text = input("Enter a string: ")
+reversed_text = text[::-1]
+print("Reversed string:", reversed_text)
+if text == reversed_text:
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+'''
+'''
+text = input("Enter a string: ")
+print("Uppercase:", text.upper())
+print("Lowercase:", text.lower())
+print("Title Case:", text.title())
+'''
+names = ["Alice", "Bob", "Charlie", "David"]
+print("Total number of names:", len(names))
